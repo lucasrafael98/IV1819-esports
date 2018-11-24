9b313e7ee8451ecce3ca58c2edf503f692617ed6
lucasrafael98 <joaolucasrafael@tecnico.ulisboa.pt>
2018-11-24 17:57:44 +0000

    Initial work on joining all idioms. Also added labels to scatter dots.

---
+++ d3/zz_main/js/heatmap.js
@@ -0,0 +1,70 @@
+let full_dataset;
+times = d3.range(24);
+d3.json("data/tourn_mmyyyy.json").then(function (data) {
+    data.data.forEach(element => {
+        element.tournaments = +element.tournaments;
+        element.startMonth = +element.startMonth;
+        element.startYear = +element.startYear;
+    });
+    full_dataset = data.data;
+    gen_heatmap();
+});
+
+function gen_heatmap(){
+    let margin = {
+        top: 0,
+        right: 25,
+        bottom: 70,
+        left: 25
+    };
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dec"];
+    let years = d3.range(2000,2018);
+
+    let width = Math.max(Math.min(window.innerWidth, 650), 500) - margin.left - margin.right - 20;
+	let gridSize = Math.floor(width / months.length);
+	let height = (gridSize) * (years.length) / 2.35;
+
+    let svg = d3.select('#heatmap')
+        .append("svg")
+        .attr("width", width + margin.left + margin.right + 100)
+        .attr("height", height + margin.top + margin.bottom + gridSize)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    let colorScale = d3.scaleLog()
+        .domain([1, d3.max(full_dataset, function(d) {return d.tournaments; })/2, d3.max(full_dataset, function(d) {return d.tournaments; })])
+        .range(["#727171", "#28638c", "#002a47"])
+
+    let monthLabels = svg.selectAll(".monthLabel")
+        .data(months)
+        .enter()
+        .append("text")
+        .text(function(d) { return d; })
+        .attr("x", function(d, i) { return (i-1.1) * gridSize; })
+        .attr("y", 0)
+        .style("text-anchor", "middle")
+        .attr("transform", "translate(115," + (height + 2.25 * gridSize)  + ")")
+
+    let yearLabels = svg.selectAll(".yearLabel")
+        .data(years)
+        .enter()
+        .append("text")
+        .text(function(d) { return d; })
+        .attr("x", 10)
+        .attr("y", function(d, i) { return (i-0.9) * gridSize /2; })
+        .style("text-anchor", "end")
+        .attr("transform", "translate(" + gridSize / 2 + ", 57.5)");
+
+    let heatMap = svg.selectAll(".hour")
+        .data(full_dataset)
+        .enter().append("rect")
+        .attr("x", function(d) { return (d.startMonth) * gridSize; })
+        .attr("y", function(d) { return (d.startYear - 1999) * gridSize / 2; })
+        .attr("width", gridSize)
+        .attr("height", gridSize / 2)
+        .style("stroke", "black")
+        .style("stroke-opacity", 0.6)
+        .style("fill", function(d) { 
+            if(d.tournaments === 0) return "#848484";
+            else return colorScale(d.tournaments); });
+}
---
+++ d3/zz_main/js/scatter.js
@@ -0,0 +1,311 @@
+var countryStats;
+var scplYMode = false;
+var scplXMode = false;
+
+d3.json("data/countries.json").then(function (data) {
+    countryStats = data.countries;
+    gen_scatter();
+});
+
+function gen_scatter(){
+    var padding = 50;
+    var w = window.innerWidth / 2.5;
+    var h = w * 2.3 / 4;
+    var yscale = d3.scaleLinear()
+        .domain([0,1])
+        .range([h - padding, padding]);
+    var xscale = d3.scaleLog()
+        .domain([1,1000])
+        .range([padding + 10,w-padding]);
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    var svg = d3.select("#scatter");
+
+    var seriesNames = d3.keys(countryStats[0])
+                            .filter(function(d) { return d === "urbanPopPercentage" 
+                                                    || d === "unemploymentPercentage"; })
+                            .sort();
+
+    // Map the data to an array of arrays of {x, y} tuples.
+    var series = seriesNames.map(function(series) {
+        return countryStats.map(function(d) {
+        return {x: +d.players, y: +d[series], title: d.countryName, code: d.countryCode, i: series};
+        });
+    });
+
+    svg = svg.insert("svg", ":first-child");
+    svg = svg.attr("width",w);
+    svg = svg.attr("height",h); 
+
+    // Default: UrbanPop/Unemployment
+    svg.selectAll(".series")
+        .data(series)
+        .enter()
+        .append("g")
+        .attr("fill", function(d, i) { return color(i); })
+        .attr("class", function(d, i) { return ("scpl-circle" + i); })
+        .selectAll(".point")
+        .data(function(d) { return d; })
+        .enter().append("circle")
+        .attr("cx",function(d) {
+            return xscale(d.x);
+        })
+        .attr("cy",function(d) {
+            return yscale(d.y / 100);
+        })
+        .attr("r", 4.5)
+        .attr("id", function(d) { return d.code; })
+        .on("mouseover", function(){
+            d3.select(this)
+                .transition()
+                .duration(50)
+                .attr("r", 6.5);
+        })
+        .on("mouseout", function(){
+            d3.select(this)
+                .transition()
+                .duration(1800)
+                .attr("r", 4.5);
+        })
+        .append("title")
+        .text(function(d) {
+            if(d.i === "urbanPopPercentage")
+                return ("Country:\t" + d.title + "\nPlayers:\t" + d.x + "\nUrban Population:\t" + d3.format(".1%")(d.y / 100));
+            else
+                return ("Country:\t" + d.title + "\nPlayers:\t" + d.x + "\nUneployment:\t" + d3.format(".1%")(d.y / 100));
+        });
+    
+    // axes
+    var yaxis = d3.axisLeft()
+        .scale(yscale)
+        .tickFormat(d3.format(".0%"))
+        .ticks(10);
+    svg.append("g")
+        .attr("transform","translate(" + padding + ",0)")
+        .attr("class","scatter-y")
+        .call(yaxis);
+    var xaxis = d3.axisBottom()
+        .scale(d3.scaleLog()
+            .domain([1, 1000])
+            .range([padding + 10, w-padding]))
+        .tickFormat(d3.format("d"))
+        .ticks(2.5);
+    svg.append("g")
+        .attr("transform","translate(0," + (h-padding) + ")")
+        .attr("class","scatter-x")
+        .call(xaxis);
+
+
+    // Switch Y axis
+    d3.selectAll("#togg-y")
+        .on("click", function() {
+            if(!scplYMode){
+                yscale = d3.scaleLog()
+                    .domain([1e9,1E14])
+                    .range([h - padding, padding]);
+            
+                svg.selectAll(".scpl-circle0")
+                    .selectAll("circle")
+                    .data(countryStats)
+                    .transition()
+                    .duration(700)
+                    .attr("fill", "gold")
+                    .attr("cy", function(d){
+                                    return yscale(d.annualGDPUSD / 1.2)})
+                    .select("title")
+                    .text(function(d) {
+                        if(!scplXMode)
+                            return ("Country:\t" + d.countryName + "\nPlayers:\t" + d.players +
+                                        "\nGDP:\t" + d3.format("$,d")(d.annualGDPUSD / 100)); 
+                        else
+                            return ("Country:\t" + d.countryName + "\nEarnings:\t" + d3.format("$,d")(d.totalUSDPrize) +
+                                        "\nGDP:\t" + d3.format("$,d")(d.annualGDPUSD / 100));   
+                    });
+
+                
+                svg.selectAll(".scpl-circle1")
+                    .selectAll("circle")
+                    .data(countryStats)
+                    .transition()
+                    .duration(700)
+                    .attr("fill", "gold")
+                    .attr("cy", function(d){
+                                    return yscale(d.annualGDPUSD / 1.2)})
+                    .select("title")
+                    .text(function(d) {
+                        if(!scplXMode)
+                            return ("Country:\t" + d.countryName + "\nPlayers:\t" + d.players +
+                                        "\nGDP:\t" + d3.format("$,d")(d.annualGDPUSD / 100)); 
+                        else
+                            return ("Country:\t" + d.countryName + "\nEarnings:\t" + d3.format("$,d")(d.totalUSDPrize) +
+                                        "\nGDP:\t" + d3.format("$,d")(d.annualGDPUSD / 100));   
+                    });
+                
+                yaxis = d3.axisLeft()
+                    .scale(yscale)
+                    .tickFormat(d3.format("$.0"))
+                    .ticks(5);
+                svg.selectAll(".scatter-y")
+                    .transition()
+                    .duration(700)
+                    .attr("transform","translate(" + padding + ",0)")
+                    .call(yaxis);
+            }
+            else{
+                yscale = d3.scaleLinear()
+                        .domain([0,1])
+                        .range([h - padding, padding]);
+
+                svg.selectAll(".scpl-circle0")
+                    .selectAll("circle")
+                    .data(series[0])
+                    .transition()
+                    .duration(700)
+                    .attr("fill", function(d, i) { return color(0); })
+                    .attr("cy", function(d){
+                                    return yscale(d.y / 100)})
+                    .select("title")
+                    .text(function(d) {
+                        if(!scplXMode)
+                            return ("Country:\t" + d.title + "\nPlayers:\t" + d.x +
+                                        "\nUnemployment:\t" + d3.format(".1%")(d.y / 100));
+                        else
+                            return ("Country:\t" + d.title + "\nEarnings:\t" + d3.format("$,d")(d.totalUSDPrize) +
+                                        "\nUnemployment:\t" + d3.format(".1%")(d.y / 100)); 
+                    });
+
+                svg.selectAll(".scpl-circle1")
+                    .selectAll("circle")
+                    .data(series[1])
+                    .transition()
+                    .duration(700)
+                    .attr("fill", function(d, i) { return color(1); })
+                    .attr("cy", function(d){
+                                    return yscale(d.y / 100)})
+                    .select("title")
+                    .text(function(d) {
+                        if(!scplXMode)
+                            return ("Country:\t" + d.title + "\nPlayers:\t" + d.x +
+                                        "\nUrban Population:\t" + d3.format(".1%")(d.y / 100));
+                        else
+                            return ("Country:\t" + d.title + "\nEarnings:\t" + d3.format("$,d")(d.totalUSDPrize) +
+                                        "\nUrban Population:\t" + d3.format(".1%")(d.y / 100)); 
+                    });
+                
+                yaxis = d3.axisLeft()
+                    .scale(yscale)
+                    .tickFormat(d3.format(".0%"))
+                    .ticks(10);
+                svg.selectAll(".scatter-y")
+                    .transition()
+                    .duration(700)
+                    .attr("transform","translate(" + padding + ",0)")
+                    .call(yaxis);
+            }
+            scplYMode = !scplYMode;
+        })
+
+
+    // Switch X axis
+    d3.selectAll("#togg-x")
+        .on("click", function(){
+            if(!scplXMode){
+                xscale = d3.scaleLog()
+                            .domain([1E4,1E8])
+                            .range([padding+ 10,w-padding]);
+                
+                svg.selectAll(".scpl-circle0")
+                    .selectAll("circle")
+                    .data(countryStats)
+                    .transition()
+                    .duration(700)
+                    .attr("cx", function(d){
+                            return xscale(d.totalUSDPrize)})
+                    .select("title")
+                    .text(function(d) {
+                        if(!scplYMode)
+                            return ("Country:\t" + d.countryName + "\nEarnings:\t" + d3.format("$,d")(d.totalUSDPrize) +
+                                        "\nUnemployment:\t" + d3.format(".1%")(d.unemploymentPercentage / 100));
+                        else
+                            return ("Country:\t" + d.countryName + "\nEarnings:\t" + d3.format("$,d")(d.totalUSDPrize) +
+                                        "\nGDP:\t" + d3.format("$,d")(d.annualGDPUSD / 100));   
+                    });
+
+                svg.selectAll(".scpl-circle1")
+                    .selectAll("circle")
+                    .data(countryStats)
+                    .transition()
+                    .duration(700)
+                    .attr("cx", function(d){
+                            return xscale(d.totalUSDPrize)})
+                    .select("title")
+                    .text(function(d) {
+                        if(!scplYMode)
+                            return ("Country:\t" + d.countryName + "\nEarnings:\t" + d3.format("$,d")(d.totalUSDPrize) +
+                                        "\nUrban Population:\t" + d3.format(".1%")(d.urbanPopPercentage / 100));
+                        else
+                            return ("Country:\t" + d.countryName + "\nEarnings:\t" + d3.format("$,d")(d.totalUSDPrize) +
+                                        "\nGDP:\t" + d3.format("$,d")(d.annualGDPUSD / 100));   
+                    });
+                
+                xaxis = d3.axisBottom()
+                    .scale(xscale)
+                    .tickFormat(d3.format("$.0"))
+                    .ticks(3);
+                svg.selectAll(".scatter-x")
+                    .transition()
+                    .duration(700)
+                    .attr("transform","translate(0, " + (h-padding) + ")")
+                    .call(xaxis);
+            }
+            else{
+                xscale = d3.scaleLog()
+                            .domain([1,1000])
+                            .range([padding + 10,w-padding]);
+    
+                svg.selectAll(".scpl-circle0")
+                    .selectAll("circle")
+                    .data(countryStats)
+                    .transition()
+                    .duration(700)
+                    .attr("cx", function(d){
+                            return xscale(d.players)})
+                    .select("title")
+                    .text(function(d) {
+                        if(!scplYMode)
+                            return ("Country:\t" + d.countryName + "\nPlayers:\t" + d.players +
+                                        "\nUnemployment:\t" + d3.format(".1%")(d.unemploymentPercentage / 100));
+                        else
+                            return ("Country:\t" + d.countryName + "\nPlayers:\t" + d.players +
+                                        "\nGDP:\t" + d3.format("$,d")(d.annualGDPUSD / 100));
+                    });
+
+                svg.selectAll(".scpl-circle1")
+                    .selectAll("circle")
+                    .data(countryStats)
+                    .transition()
+                    .duration(700)
+                    .attr("cx", function(d){
+                            return xscale(d.players)})
+                    .select("title")
+                    .text(function(d) {
+                        if(!scplYMode)
+                            return ("Country:\t" + d.countryName + "\nPlayers:\t" + d.players +
+                                        "\nUrban Population:\t" + d3.format(".1%")(d.urbanPopPercentage / 100));
+                        else
+                            return ("Country:\t" + d.countryName + "\nPlayers:\t" + d.players +
+                                        "\nGDP:\t" + d3.format("$,d")(d.annualGDPUSD / 100));   
+                    });
+                
+                xaxis = d3.axisBottom()
+                    .scale(xscale)
+                    .tickFormat(d3.format("d"))
+                    .ticks(2.5);
+                svg.selectAll(".scatter-x")
+                    .transition()
+                    .duration(700)
+                    .attr("transform","translate(0, " + (h-padding) + ")")
+                    .call(xaxis);
+            }
+            scplXMode = !scplXMode;
+        });
+}
